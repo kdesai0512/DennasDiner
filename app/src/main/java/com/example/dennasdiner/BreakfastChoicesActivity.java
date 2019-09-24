b5f3c04a770a09aec1a6c492a71dc2cb2af9d80b
kdesai0512 <desai8252@students.d211.org>
2019-09-23 22:02:11 -0500

    Finish Breakfast. Errors within BreakfastChoicesActivity. End of Mon.

--- app/src/main/java/com/example/dennasdiner/BreakfastChoicesActivity.java
+++ app/src/main/java/com/example/dennasdiner/BreakfastChoicesActivity.java
@@ -1,8 +1,13 @@
-package com.example.dennasdiner;
+package com.example.dennasdinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class BreakfastChoicesActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class BreakfastChoicesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfast_choices);
+
+
+        ArrayAdapter<Food> listAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, Food.breakfastFoods);
+
+        ListView listFoods = (ListView) findViewById(R.id.breakfastChoices);
+        listFoods.setAdapter(listAdapter);
+
+        // Create a listener to listen for when a Food item is clicked on
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> listFoods,
+                                            View itemView, int position, long id) {
+
+                        // Pass the Food name the user clicks on to BreakfastChoicesActivity
+                        Intent intent = new Intent(BreakfastChoicesActivity.this,
+                                BreakfastItemDetailActivity.class);
+                        intent.putExtra(BreakfastItemDetailActivity.EXTRA_FOOD_ID, (int) id);
+                        startActivity(intent);
+                    }
+                };
+        // Assign the listener to the list view
+        listFoods.setOnItemClickListener(itemClickListener);
     }
 }
--- app/src/main/java/com/example/dennasdiner/BreakfastItemDetailActivity.java
+++ app/src/main/java/com/example/dennasdiner/BreakfastItemDetailActivity.java
@@ -2,13 +2,34 @@ package com.example.dennasdiner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BreakfastItemDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_FOOD_ID = "foodChoice";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_breakfast_item_detail);
+
+        Intent intent = getIntent();
+        int foodId = intent.getIntExtra(EXTRA_FOOD_ID, 0);
+
+        Food myFood = Food.breakfastFoods[foodId];
+
+        ImageView photo = (ImageView) findViewById(R.id.food_pic);
+        TextView name = (TextView) findViewById(R.id.food_name);
+        TextView desc = (TextView) findViewById(R.id.food_desc);
+        TextView price = (TextView) findViewById(R.id.food_price);
+
+        photo.setImageResource((myFood.getImageResourceID()));
+        name.setText(myFood.getFoodName());
+        desc.setText(myFood.getDescription());
+        price.setText("$" + myFood.getPrice());
     }
 }
+
